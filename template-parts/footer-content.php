10905245f437679d69ee45c189a17dd15c47b8f9
Teodor Simionescu <simionescu.mails@gmail.com>
2024-03-12 19:34:04 +0200

    first try

---
+++ template-parts/footer-content.php
@@ -0,0 +1,5 @@
+<footer class="u-align-center u-clearfix u-footer u-grey-10 u-footer" id="sec-1e43">
+  <div class="u-clearfix u-sheet u-sheet-1">
+    <p class="u-small-text u-text u-text-variant u-text-1">©Pexil - firmă construcţii din Suceava </p>
+  </div>
+</footer>
